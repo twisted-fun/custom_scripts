35726085b57884df974646a49e9cdb5d2d7ad6c5
r00t3r <pentesting4fun@gmail.com>
2019-02-19 12:56:25 -0800

    added some more lambdas in skull

--- skull.py
+++ skull.py
@@ -30,6 +30,11 @@ sla = lambda x, y : r.sendlineafter(x, y)
 sa = lambda x, y : r.sendafter(x, y)
 rl = lambda : r.recvline()
 ru = lambda x : r.recvuntil(x)
+l64 = lambda : u64(r.recv(6) + "\x00"*2)
+ll = lambda x, y : log.info(x + ": 0x{:x}".format(y))
+li = lambda x : log.info(x)
+ls = lambda x : log.success(x)
+ga = lambda : gdb.attach(r)
 ###---------- XXX ----------###
 
 
